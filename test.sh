484384623da248e42fa8d8741d40535b5ef77868
Jonathan Goldfarb <jgoldfar@gmail.com>
2019-03-02 07:18:35 -0500

    Add informative status message to test script

--- test.sh
+++ test.sh
@@ -2,11 +2,11 @@
 set -euo pipefail
 IFS=$'\n\t'
 
-# Run notebook (nbconvert) example
+echo "Run notebook (nbconvert) example"
 docker run --rm --workdir /home/fenics --volume "$(pwd)":/home/fenics/shared jgoldfar/fenics:jupyter-nbconvert --to notebook --execute --output ex-check.ipynb shared/test/demo.ipynb
 
-# Run FEniCS example
+echo "Run FEniCS example"
 docker run --rm --workdir /home/fenics --volume "$(pwd)":/home/fenics/shared jgoldfar/fenics:base python3 demo/python/documented/neumann-poisson/demo_neumann-poisson.py
 
-# Run example using code in local (external to the image) directory
-docker run --rm --workdir /home/fenics --volume "$(pwd)":/home/fenics/shared jgoldfar/fenics:base python3 shared/test/testcode.p
+echo "Run example using code in local (external to the image) directory"
+docker run --rm --workdir /home/fenics --volume "$(pwd)":/home/fenics/shared jgoldfar/fenics:base python3 shared/test/testcode.py
